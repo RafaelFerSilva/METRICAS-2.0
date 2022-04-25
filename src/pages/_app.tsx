0811add74bd6e576fb6d89ce78da262f7dfb8694
Rafael Fernandes da Silva <81975862+Rafael-Orbia@users.noreply.github.com>
2022-04-25 09:11:24 -0300

    remove react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,17 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "../styles/theme";
 import { SidebarDrawerProvider } from "../contexts/SidebarDraweContext";
-import { QueryClientProvider, QueryClient } from "react-query";
-import { queryClient } from "../services/queryCliente";
 import { TeamsProvider } from "../contexts/TeamsContext";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
         <TeamsProvider>
-          <QueryClientProvider client={queryClient}>
             <ChakraProvider theme={theme}>
               <SidebarDrawerProvider>
                 <Component {...pageProps} />
               </SidebarDrawerProvider>
             </ChakraProvider>
-            <ReactQueryDevtools />
-          </QueryClientProvider>
         </TeamsProvider>
     </>
   );
