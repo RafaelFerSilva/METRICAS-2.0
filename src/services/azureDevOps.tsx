a61a283e9743f04c98ad3bb00f57565d868893b6
Rafael Fernandes da Silva <rafael.silva@orbia.ag>
2024-10-31 15:56:11 -0300

    add query validation

--- src/services/azureDevOps.tsx
+++ src/services/azureDevOps.tsx
@@ -81,6 +81,7 @@ export const fetchTestSuites = async (planId: number) => {
                [System.State] 
         FROM WorkItems 
         WHERE [System.WorkItemType] = 'Test Case' 
+          AND [System.AreaPath] != 'Engineering\\Qualidade'
           AND [Custom.ec38de40-257b-4c45-9db9-284080382c3e] != 'Não passível de automação'
       `
     };
